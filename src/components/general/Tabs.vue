45a5385b32f997ba07fb77c92f211434de8e58e1
Patricio Saldia <psaldias@mandrildigital.cl>
2022-12-29 16:46:58 -0300

    fix tabs equipos

--- src/components/general/Tabs.vue
+++ src/components/general/Tabs.vue
@@ -1,53 +1,56 @@
-<template >
-    <main v-if="tabs">
-        <div class="tabs" >
-            <ul>
-                <li v-for="tab, index in tabsConInformacion" :key="index" :class="{ 'is-active': activo == index }">
-                    <a href="#" @click.prevent="cambiarActivo(index)">{{tab.titulo}}</a>
-                </li>
-            </ul>
-        </div>
-        <div class="tabs-content content" >
-            <div class="columns">
-                <div class="column" v-html="tabActivo.descripcion"></div>
-                <div class="column" v-if="tabActivo.descripcion2" v-html="tabActivo.descripcion2"></div>
-            </div>
-
-        </div>
-   </main>
+<template>
+  <main v-if="tabs">
+    <div class="tabs">
+      <ul>
+        <li
+          v-for="(tab, index) in tabsConInformacion"
+          :key="index"
+          :class="{ 'is-active': activo == index }"
+        >
+          <a href="#" @click.prevent="cambiarActivo(index)">{{ tab.titulo }}</a>
+        </li>
+      </ul>
+    </div>
+    <div class="tabs-content content">
+      <div class="columns">
+        <div class="column" v-html="tabActivo.descripcion"></div>
+        <div
+          class="column"
+          v-if="tabActivo.descripcion2"
+          v-html="tabActivo.descripcion2"
+        ></div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-    props: {
-        tabs: {}
+  props: {
+    tabs: {},
+  },
+  data() {
+    return {
+      activo: 0,
+    };
+  },
+  computed: {
+    tabActivo() {
+      if (this.tabsConInformacion)
+        return this.tabsConInformacion.find((tab, index) => index == this.activo);
+      return false;
     },
-    data() {
-        return {
-            activo : 0
-        }
+    tabsConInformacion() {
+      if (this.tabs) return this.tabs.filter((tab, index) => tab.descripcion != "");
+      return false;
     },
-    computed: {
-        tabActivo(){
-            if(this.tabs)
-                return this.tabs.find( (tab,index) => index == this.activo);
-            return false;
-
-        },
-        tabsConInformacion(){
-            if(this.tabs)
-                return this.tabs.filter( (tab,index) => tab.descripcion != '');
-            return false;
-
-        }
+  },
+  methods: {
+    cambiarActivo(id) {
+      if (this.activo == id) {
+        this.activo = 0;
+      } else this.activo = id;
     },
-    methods: {
-        cambiarActivo(id) {
-            if (this.activo == id) {
-                this.activo = 0;
-            } else
-                this.activo = id;
-        }
-    }
-}
+  },
+};
 </script>
